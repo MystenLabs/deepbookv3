c90a47c893ec9910f35e6375727a8bf50d7b515e
Aslan Tashtanov <aslan.tashtanov@mystenlabs.com>
2024-09-26 10:11:15 -0400

    changes

--- packages/market_maker/main.ts
+++ packages/market_maker/main.ts
@@ -7,10 +7,22 @@ import { MarketMaker } from "./marketMaker";
     await mm.checkBalances();
 
     // Deposit 1.0 SUI, 1.0 DEEP, 0 DBUSDC, 0 DBUSDT
+    // const tx = new Transaction();
+    // await mm.depositCoins(100,100,100,100);
+    // await mm.signAndExecute(tx);
+    // await mm.checkBalances();
+    // const tx = new Transaction();
+    // await mm.borrowAndReturnFlashloan(tx, "DEEP_SUI", 1);
+    // await mm.signAndExecute(tx);
+
     const tx = new Transaction();
-    await mm.depositCoins(1, 1, 0, 0);
+    await mm.placeOrder(tx, "DEEP_SUI", 0.98, 10, true);
+    // await mm.placeOrder(tx, "DEEP_SUI", 1.02, 1, true);
+    // await mm.placeOrdersAroundMid(tx, "DEEP_SUI", 10, 25, deepsuiPrice);
+    // await mm.placeOrdersAroundMid(tx, "SUI_DBUSDC", 10, 25, suiPrice);
+    // await mm.placeOrdersAroundMid(tx, "DEEP_DBUSDC", 10, 25, 1);
+    // await mm.placeOrdersAroundMid(tx, "DBUSDT_DBUSDC", 10, 25, 1);
     await mm.signAndExecute(tx);
-    await mm.checkBalances();
     
     // withdraw all SUI, DEEP, DBUSDC, DBUSDT
     // await mm.withdrawCoins();
