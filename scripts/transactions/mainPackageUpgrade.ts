f07966bf94f12c611ef62fc6559d1d635143267d
Tony Lee <chunglee2018@u.northwestern.edu>
2024-10-08 10:07:08 -0400

    dotenv remove

--- scripts/transactions/mainPackageUpgrade.ts
+++ scripts/transactions/mainPackageUpgrade.ts
@@ -2,24 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { execSync } from 'child_process';
-import dotenv from 'dotenv';
 
 import { upgradeCapID } from '../config/constants';
 
-dotenv.config();
-
-const gasObject = "0x015c81bb048561bedbf687920c1a623ecdf3df4e91236d086dde8b564c3a3f61";
 const network = 'testnet';
 
 // Active env of sui has to be the same with the env we're publishing to.
 // if upgradeCap & gasObject is on mainnet, it has to be on mainnet.
 // Github actions are always on mainnet.
 const mainPackageUpgrade = async () => {
-	if (!gasObject) throw new Error('Gas Object not supplied for a mainnet transaction');
-
 	// on GH Action, the sui binary is located on root. Referencing that as `/` doesn't work.
 	const suiFolder = process.env.ORIGIN === 'gh_action' ? '../../sui' : 'sui';
-	const upgradeCall = `${suiFolder} client upgrade --upgrade-capability ${upgradeCapID[network]} --gas-budget 3000000000 --gas ${gasObject} --skip-dependency-verification --serialize-unsigned-transaction`;
+	const upgradeCall = `${suiFolder} client upgrade --upgrade-capability ${upgradeCapID[network]} --gas-budget 3000000000 --skip-dependency-verification --serialize-unsigned-transaction`;
 
 	// we execute this on `setup/package.json` so we go one level back, access packages folder -> deepbook -> upgrade.
 	// we go from scripts/(base)/packages/deepbook, we run the upgrade and then we save the transaction data
