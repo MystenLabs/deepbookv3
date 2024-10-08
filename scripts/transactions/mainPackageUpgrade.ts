4c2071131683e0f549bf3692027f03e3b91b16d3
Tony Lee <chunglee2018@u.northwestern.edu>
2024-10-08 15:55:30 -0400

    upgrade

--- scripts/transactions/mainPackageUpgrade.ts
+++ scripts/transactions/mainPackageUpgrade.ts
@@ -6,15 +6,19 @@ import { execSync } from 'child_process';
 import { upgradeCapID } from '../config/constants';
 
 const network = 'mainnet';
-const gasObject = '0xb76abcaefff13813adfa61768fac06a13a9bdbe7c86fb75118885364452286cb'
 
 // Active env of sui has to be the same with the env we're publishing to.
 // if upgradeCap & gasObject is on mainnet, it has to be on mainnet.
 // Github actions are always on mainnet.
 const mainPackageUpgrade = async () => {
 	// on GH Action, the sui binary is located on root. Referencing that as `/` doesn't work.
+    const gasObjectId = process.env.GAS_OBJECT;
+
+	// enabling the gas Object check only on mainnet, to allow testnet multisig tests.
+	if (!gasObjectId) throw new Error('No gas object supplied for a mainnet transaction');
+
     const suiFolder = process.env.ORIGIN === 'gh_action' ? '/home/linuxbrew/.linuxbrew/bin/sui' : 'sui';
-	const upgradeCall = `${suiFolder} client upgrade --upgrade-capability ${upgradeCapID[network]} --gas-budget 3000000000 --gas ${gasObject} --skip-dependency-verification --serialize-unsigned-transaction`;
+	const upgradeCall = `${suiFolder} client upgrade --upgrade-capability ${upgradeCapID[network]} --gas-budget 3000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
 
     execSync(`cd $PWD/../packages/deepbook && ${upgradeCall} > $PWD/../../scripts/tx/tx-data.txt`);
 };
