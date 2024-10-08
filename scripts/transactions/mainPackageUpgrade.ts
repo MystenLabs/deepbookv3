42b3d0c0aaa967fcc0663f15631ef92119878d4e
Tony Lee <chunglee2018@u.northwestern.edu>
2024-10-08 16:13:46 -0400

    hi

--- scripts/transactions/mainPackageUpgrade.ts
+++ scripts/transactions/mainPackageUpgrade.ts
@@ -17,11 +17,14 @@ const mainPackageUpgrade = async () => {
     // Enabling the gas Object check only on mainnet, to allow testnet multisig tests.
     if (!gasObjectId) throw new Error('No gas object supplied for a mainnet transaction');
 
+    const suiPath = process.env.ORIGIN === 'gh_action'
+    ? '/home/linuxbrew/.linuxbrew/bin/sui'
+    : 'sui';
     const packagesDir = path.resolve(__dirname, '../../packages/deepbook');
     const txFilePath = path.resolve(__dirname, '../../scripts/tx/tx-data.txt');
 
     // Construct the command
-    const upgradeCall = `sui client upgrade --upgrade-capability ${upgradeCapID[network]} --gas-budget 3000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
+    const upgradeCall = `${suiPath} client upgrade --upgrade-capability ${upgradeCapID[network]} --gas-budget 3000000000 --gas ${gasObjectId} --skip-dependency-verification --serialize-unsigned-transaction`;
 
     try {
         // Execute the command with the specified working directory and capture the output
