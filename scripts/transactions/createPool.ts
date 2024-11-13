009ac2e04af7ef04a4afd969b730ba475df078f5
Tony Lee <chunglee2018@u.northwestern.edu>
2024-11-13 11:22:18 -0500

    create ns pools and package updates

--- scripts/transactions/createPool.ts
+++ scripts/transactions/createPool.ts
@@ -1,76 +1,56 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-import { Transaction } from '@mysten/sui/transactions';
-import { prepareMultisigTx } from '../utils/utils';
-import { adminCapOwner, adminCapID } from '../config/constants';
-import { DeepBookClient } from '@mysten/deepbook-v3';
-import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
+import { Transaction } from "@mysten/sui/transactions";
+import { prepareMultisigTx } from "../utils/utils";
+import { adminCapOwner, adminCapID } from "../config/constants";
+import { DeepBookClient } from "@mysten/deepbook-v3";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
 (async () => {
-	// Update constant for env
-	const env = 'mainnet';
+  // Update constant for env
+  const env = "mainnet";
 
-	// Initialize with balance managers if needed
-	const balanceManagers = {
-		MANAGER_1: {
-			address: '',
-			tradeCap: '',
-		},
-	};
+  // Initialize with balance managers if needed
+  const balanceManagers = {
+    MANAGER_1: {
+      address: "",
+      tradeCap: "",
+    },
+  };
 
-	const dbClient = new DeepBookClient({
-		address: '0x0',
-		env: env,
-		client: new SuiClient({
-			url: getFullnodeUrl(env),
-		}),
-		balanceManagers: balanceManagers,
-		adminCap: adminCapID[env],
-	});
+  const dbClient = new DeepBookClient({
+    address: "0x0",
+    env: env,
+    client: new SuiClient({
+      url: getFullnodeUrl(env),
+    }),
+    balanceManagers: balanceManagers,
+    adminCap: adminCapID[env],
+  });
 
-	const tx = new Transaction();
+  const tx = new Transaction();
 
-	dbClient.deepBookAdmin.createPoolAdmin({
-		baseCoinKey: 'SUI',
-		quoteCoinKey: 'USDC',
-		tickSize: 0.001,
-		lotSize: 0.1,
-		minSize: 1,
-		whitelisted: false,
-		stablePool: false,
-	})(tx);
+  dbClient.deepBookAdmin.createPoolAdmin({
+    baseCoinKey: "NS",
+    quoteCoinKey: "USDC",
+    tickSize: 0.00001,
+    lotSize: 0.1,
+    minSize: 1,
+    whitelisted: false,
+    stablePool: false,
+  })(tx);
 
-	dbClient.deepBookAdmin.createPoolAdmin({
-		baseCoinKey: 'WUSDT',
-		quoteCoinKey: 'USDC',
-		tickSize: 0.001,
-		lotSize: 0.1,
-		minSize: 1,
-		whitelisted: false,
-		stablePool: true,
-	})(tx);
+  dbClient.deepBookAdmin.createPoolAdmin({
+    baseCoinKey: "NS",
+    quoteCoinKey: "SUI",
+    tickSize: 0.00001,
+    lotSize: 0.1,
+    minSize: 1,
+    whitelisted: false,
+    stablePool: false,
+  })(tx);
 
-	dbClient.deepBookAdmin.createPoolAdmin({
-		baseCoinKey: 'BETH',
-		quoteCoinKey: 'USDC',
-		tickSize: 0.001,
-		lotSize: 0.0001,
-		minSize: 0.001,
-		whitelisted: false,
-		stablePool: false,
-	})(tx);
+  let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
 
-	dbClient.deepBookAdmin.createPoolAdmin({
-		baseCoinKey: 'WUSDC',
-		quoteCoinKey: 'USDC',
-		tickSize: 0.001,
-		lotSize: 0.1,
-		minSize: 1,
-		whitelisted: true,
-		stablePool: false,
-	})(tx);
-
-	let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
-
-	console.dir(res, { depth: null });
+  console.dir(res, { depth: null });
 })();
