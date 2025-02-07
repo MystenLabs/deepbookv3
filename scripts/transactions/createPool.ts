bf38e4da9d1c4d6fb482be2cd93703bd35aa3f6e
Tony Lee <chunglee2018@u.northwestern.edu>
2025-02-07 12:34:58 -0500

    giga pool action

--- scripts/transactions/createPool.ts
+++ scripts/transactions/createPool.ts
@@ -7,40 +7,40 @@ import { DeepBookClient } from "@mysten/deepbook-v3";
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
 (async () => {
-  // Update constant for env
-  const env = "mainnet";
+    // Update constant for env
+    const env = "mainnet";
 
-  // Initialize with balance managers if needed
-  const balanceManagers = {
-    MANAGER_1: {
-      address: "",
-      tradeCap: "",
-    },
-  };
+    // Initialize with balance managers if needed
+    const balanceManagers = {
+        MANAGER_1: {
+            address: "",
+            tradeCap: "",
+        },
+    };
 
-  const dbClient = new DeepBookClient({
-    address: "0x0",
-    env: env,
-    client: new SuiClient({
-      url: getFullnodeUrl(env),
-    }),
-    balanceManagers: balanceManagers,
-    adminCap: adminCapID[env],
-  });
+    const dbClient = new DeepBookClient({
+        address: "0x0",
+        env: env,
+        client: new SuiClient({
+            url: getFullnodeUrl(env),
+        }),
+        balanceManagers: balanceManagers,
+        adminCap: adminCapID[env],
+    });
 
-  const tx = new Transaction();
+    const tx = new Transaction();
 
-  dbClient.deepBookAdmin.createPoolAdmin({
-    baseCoinKey: "DRF",
-    quoteCoinKey: "SUI",
-    tickSize: 0.000001,
-    lotSize: 1,
-    minSize: 10,
-    whitelisted: false,
-    stablePool: false,
-  })(tx);
+    dbClient.deepBookAdmin.createPoolAdmin({
+        baseCoinKey: "WGIGA",
+        quoteCoinKey: "SUI",
+        tickSize: 0.000001,
+        lotSize: 0.1,
+        minSize: 1,
+        whitelisted: false,
+        stablePool: false,
+    })(tx);
 
-  let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
+    let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
 
-  console.dir(res, { depth: null });
+    console.dir(res, { depth: null });
 })();
