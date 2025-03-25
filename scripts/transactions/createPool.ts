bfcbb8f1cdebc373afaea14271e303e24711fc42
Tony Lee <tony.lee@mystenlabs.com>
2025-03-25 13:33:46 -0400

    wal pools (#346)

--- scripts/transactions/createPool.ts
+++ scripts/transactions/createPool.ts
@@ -7,40 +7,50 @@ import { DeepBookClient } from "@mysten/deepbook-v3";
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
 (async () => {
-    // Update constant for env
-    const env = "mainnet";
+  // Update constant for env
+  const env = "mainnet";
 
-    // Initialize with balance managers if needed
-    const balanceManagers = {
-        MANAGER_1: {
-            address: "",
-            tradeCap: "",
-        },
-    };
+  // Initialize with balance managers if needed
+  const balanceManagers = {
+    MANAGER_1: {
+      address: "",
+      tradeCap: "",
+    },
+  };
 
-    const dbClient = new DeepBookClient({
-        address: "0x0",
-        env: env,
-        client: new SuiClient({
-            url: getFullnodeUrl(env),
-        }),
-        balanceManagers: balanceManagers,
-        adminCap: adminCapID[env],
-    });
+  const dbClient = new DeepBookClient({
+    address: "0x0",
+    env: env,
+    client: new SuiClient({
+      url: getFullnodeUrl(env),
+    }),
+    balanceManagers: balanceManagers,
+    adminCap: adminCapID[env],
+  });
 
-    const tx = new Transaction();
+  const tx = new Transaction();
 
-    dbClient.deepBookAdmin.createPoolAdmin({
-        baseCoinKey: "SEND",
-        quoteCoinKey: "USDC",
-        tickSize: 0.000001,
-        lotSize: 0.1,
-        minSize: 1,
-        whitelisted: false,
-        stablePool: false,
-    })(tx);
+  dbClient.deepBookAdmin.createPoolAdmin({
+    baseCoinKey: "WAL",
+    quoteCoinKey: "USDC",
+    tickSize: 0.000001,
+    lotSize: 0.1,
+    minSize: 1,
+    whitelisted: false,
+    stablePool: false,
+  })(tx);
 
-    let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
+  dbClient.deepBookAdmin.createPoolAdmin({
+    baseCoinKey: "WAL",
+    quoteCoinKey: "SUI",
+    tickSize: 0.000001,
+    lotSize: 0.1,
+    minSize: 1,
+    whitelisted: false,
+    stablePool: false,
+  })(tx);
 
-    console.dir(res, { depth: null });
+  let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
+
+  console.dir(res, { depth: null });
 })();
