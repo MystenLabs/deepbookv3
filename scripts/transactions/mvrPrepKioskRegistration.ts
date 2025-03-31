210128849bb0d9bb72f3a94c393819eff763123f
Tony Lee <chunglee2018@u.northwestern.edu>
2025-03-31 11:24:26 -0500

    kiosk description

--- scripts/transactions/mvrPrepKioskRegistration.ts
+++ scripts/transactions/mvrPrepKioskRegistration.ts
@@ -90,7 +90,7 @@ export type Network = "mainnet" | "testnet" | "devnet" | "localnet";
       transaction.object(appCapObjectId),
       transaction.pure.string("description"), // key
       transaction.pure.string(
-        "Includes collection of transfer policies, kiosk extensions and libraries to work with all of them. It is meant to act as a Kiosk Sui Move monorepo with a set release cycle and a very welcoming setting for external contributions."
+        "Collection of rules and extensions for Kiosk and creator Transfer Policies."
       ), // value
     ],
   });
