6c2579bc8115da256899dd1e00587fc30a53a469
Aslan Tashtanov <aslan.tashtanov@mystenlabs.com>
2025-04-01 19:28:58 -0400

    deepbook server

---
+++ crates/server/README.md
@@ -0,0 +1,3 @@
+# DeepBook Server
+
+The DeepBook Server is a Rust application that provides a RESTful API for the DeepBook project. It allows users to interact with the DeepBook database and retrieve information about DeepBook events.
---
+++ crates/server/src/main.rs
@@ -0,0 +1,34 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use clap::Parser;
+use deepbook_server::postgres_manager::get_connection_pool;
+use deepbook_server::server::run_server;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use url::Url;
+
+#[derive(Parser)]
+#[clap(rename_all = "kebab-case", author, version)]
+struct Args {
+    #[clap(env, long, default_value_t = 9008)]
+    server_port: u16,
+    #[clap(
+        env,
+        long,
+        default_value = "postgres://postgres:postgrespw@localhost:5432/deepbook"
+    )]
+    database_url: Url,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let Args {
+        server_port,
+        database_url,
+    } = Args::parse();
+    let server_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), server_port);
+    let pg_pool = get_connection_pool(database_url).await;
+    run_server(server_address, pg_pool).await?;
+
+    Ok(())
+}
