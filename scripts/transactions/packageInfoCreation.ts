cee777549f9d43a0f26d426c4b9b03a316512a94
Tony Lee <chunglee2018@u.northwestern.edu>
2025-04-22 13:00:22 +0400

    suins denylist packageinfo

--- scripts/transactions/packageInfoCreation.ts
+++ scripts/transactions/packageInfoCreation.ts
@@ -25,8 +25,8 @@ const mainnetPlugin = namedPackagesPlugin({
     target: `@mvr/metadata::package_info::new`,
     arguments: [
       transaction.object(
-        "0x30cdbd781c027a129e0e15feb0409e950a78b376904ee66615a9d5d8d502c95b"
-      ), // Suins UpgradeCap
+        "0x72a3c603d0218ab59ae81363e608d6c3c0c344890df40bd6ca7de575f28feb7d"
+      ), // Denylist Package UpgradeCap
     ],
   });
 
@@ -53,51 +53,51 @@ const mainnetPlugin = namedPackagesPlugin({
     ],
   }); // PackageInfo transferred to MVR account
 
-  const MVRAppCaps = {
-    core: "0xf30a07fc1fadc8bd33ed4a9af5129967008201387b979a9899e52fbd852b29a9",
-    payments:
-      "0xcb44143e2921ed0fb82529ba58f5284ec77da63a8640e57c7fa8c12e87fa8baf",
-    subnames:
-      "0x969978eba35e57ad66856f137448da065bc27962a1bc4a6dd8b6cc229c899d5a",
-    coupons:
-      "0x4f3fa0d4da16578b8261175131bc7a24dcefe3ec83b45690e29cbc9bb3edc4de",
-    discounts:
-      "0x327702a5751c9582b152db81073e56c9201fad51ecbaf8bb522ae8df49f8dfd1",
-    tempSubnameProxy:
-      "0x3b2582036fe9aa17c059e7b3993b8dc97ae57d2ac9e1fe603884060c98385fb2",
-  };
+  // const MVRAppCaps = {
+  //   core: "0xf30a07fc1fadc8bd33ed4a9af5129967008201387b979a9899e52fbd852b29a9",
+  //   payments:
+  //     "0xcb44143e2921ed0fb82529ba58f5284ec77da63a8640e57c7fa8c12e87fa8baf",
+  //   subnames:
+  //     "0x969978eba35e57ad66856f137448da065bc27962a1bc4a6dd8b6cc229c899d5a",
+  //   coupons:
+  //     "0x4f3fa0d4da16578b8261175131bc7a24dcefe3ec83b45690e29cbc9bb3edc4de",
+  //   discounts:
+  //     "0x327702a5751c9582b152db81073e56c9201fad51ecbaf8bb522ae8df49f8dfd1",
+  //   tempSubnameProxy:
+  //     "0x3b2582036fe9aa17c059e7b3993b8dc97ae57d2ac9e1fe603884060c98385fb2",
+  // };
 
-  const packageInfos = {
-    core: "0xf709e4075c19d9ab1ba5acb17dfbf08ddc1e328ab20eaa879454bf5f6b98758e",
-    payments:
-      "0xa46d971d0e9298488605e1850d64fa067db9d66570dda8dad37bbf61ab2cca21",
-    subnames:
-      "0x9470cf5deaf2e22232244da9beeabb7b82d4a9f7b9b0784017af75c7641950ee",
-    coupons:
-      "0xf7f29dce2246e6c79c8edd4094dc3039de478187b1b13e871a6a1a87775fe939",
-    discounts:
-      "0xcb8d0cefcda3949b3ff83c0014cb50ca2a7c7b2074a5a7c1f2fce68cb9ad7dd6",
-    tempSubnameProxy:
-      "0x9accbc6d7c86abf91dcbe247fd44c6eb006d8f1864ff93b90faaeb09114d3b6f",
-  };
+  // const packageInfos = {
+  //   core: "0xf709e4075c19d9ab1ba5acb17dfbf08ddc1e328ab20eaa879454bf5f6b98758e",
+  //   payments:
+  //     "0xa46d971d0e9298488605e1850d64fa067db9d66570dda8dad37bbf61ab2cca21",
+  //   subnames:
+  //     "0x9470cf5deaf2e22232244da9beeabb7b82d4a9f7b9b0784017af75c7641950ee",
+  //   coupons:
+  //     "0xf7f29dce2246e6c79c8edd4094dc3039de478187b1b13e871a6a1a87775fe939",
+  //   discounts:
+  //     "0xcb8d0cefcda3949b3ff83c0014cb50ca2a7c7b2074a5a7c1f2fce68cb9ad7dd6",
+  //   tempSubnameProxy:
+  //     "0x9accbc6d7c86abf91dcbe247fd44c6eb006d8f1864ff93b90faaeb09114d3b6f",
+  // };
 
-  // Transfer all app cap + package info objects
-  const allAppCaps: string[] = [];
+  // // Transfer all app cap + package info objects
+  // const allAppCaps: string[] = [];
 
-  for (const value of Object.values(MVRAppCaps)) {
-    allAppCaps.push(value);
-  }
-  transaction.transferObjects(allAppCaps, holdingAddress);
+  // for (const value of Object.values(MVRAppCaps)) {
+  //   allAppCaps.push(value);
+  // }
+  // transaction.transferObjects(allAppCaps, holdingAddress);
 
-  for (const packageInfoId of Object.values(packageInfos)) {
-    transaction.moveCall({
-      target: `@mvr/metadata::package_info::transfer`,
-      arguments: [
-        transaction.object(packageInfoId),
-        transaction.pure.address(holdingAddress),
-      ],
-    }); // PackageInfo transferred to MVR account
-  }
+  // for (const packageInfoId of Object.values(packageInfos)) {
+  //   transaction.moveCall({
+  //     target: `@mvr/metadata::package_info::transfer`,
+  //     arguments: [
+  //       transaction.object(packageInfoId),
+  //       transaction.pure.address(holdingAddress),
+  //     ],
+  //   }); // PackageInfo transferred to MVR account
+  // }
 
   let res = await prepareMultisigTx(
     transaction,
