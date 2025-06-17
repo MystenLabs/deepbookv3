5aed9139a2473236f31a143a78a7449dfbb61524
Patrick Kuo <patrickkuo@me.com>
2025-06-17 15:52:49 +0100

    add metrics to deepbook api (#396)

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -4,14 +4,19 @@
 use clap::Parser;
 use deepbook_server::server::run_server;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use sui_pg_db::{Db, DbArgs};
+use sui_pg_db::DbArgs;
+use tokio_util::sync::CancellationToken;
 use url::Url;
 
 #[derive(Parser)]
 #[clap(rename_all = "kebab-case", author, version)]
 struct Args {
+    #[command(flatten)]
+    db_args: DbArgs,
     #[clap(env, long, default_value_t = 9008)]
     server_port: u16,
+    #[clap(env, long, default_value = "0.0.0.0:9184")]
+    metrics_address: SocketAddr,
     #[clap(
         env,
         long,
@@ -25,13 +30,24 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let Args {
+        db_args,
         server_port,
+        metrics_address,
         database_url,
         rpc_url,
     } = Args::parse();
     let server_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), server_port);
-    let db = Db::for_read(database_url, DbArgs::default()).await?;
-    run_server(server_address, db, rpc_url).await?;
+    let cancel = CancellationToken::new();
+
+    run_server(
+        server_address,
+        database_url,
+        db_args,
+        rpc_url,
+        cancel.child_token(),
+        metrics_address,
+    )
+    .await?;
 
     Ok(())
 }
