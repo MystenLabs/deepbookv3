e15cfb0016132bf06a2c5abfac9496d558b7bdf9
Tom Harbert <tom@mystenlabs.com>
2025-09-27 19:36:40 -0700

    handle null first checkpoint arg

--- docker/deepbook-indexer/entry.sh
+++ docker/deepbook-indexer/entry.sh
@@ -3,4 +3,9 @@
 export RUST_BACKTRACE=1
 export RUST_LOG=debug
 
-/opt/mysten/bin/deepbook-indexer --database-url "$DATABASE_URL" --env "$NETWORK" --first-checkpoint "$FIRST_CHECKPOINT"
+# if FIRST_CHECKPOINT is set, include it, otherwise exclude it
+if [ -n "$FIRST_CHECKPOINT" ]; then
+    /opt/mysten/bin/deepbook-indexer --database-url "$DATABASE_URL" --env "$NETWORK" --first-checkpoint "$FIRST_CHECKPOINT"
+else
+    /opt/mysten/bin/deepbook-indexer --database-url "$DATABASE_URL" --env "$NETWORK"
+fi
