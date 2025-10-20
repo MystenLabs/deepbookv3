1c3c4628cd8e54119bbbab4207babde7e0c26b82
Sam Barani <sam.barani@mystenlabs.com>
2025-10-20 15:44:28 -0500

    add testnet packages (#612)
    * add testnet packages

--- crates/indexer/src/lib.rs
+++ crates/indexer/src/lib.rs
@@ -9,14 +9,19 @@ pub(crate) mod models;
 pub const MAINNET_REMOTE_STORE_URL: &str = "https://checkpoints.mainnet.sui.io";
 pub const TESTNET_REMOTE_STORE_URL: &str = "https://checkpoints.testnet.sui.io";
 
-// Previous package IDs for mainnet
 const MAINNET_PREVIOUS_PACKAGES: &[&str] = &[
     "0x2c8d603bc51326b8c13cef9dd07031a408a48dddb541963357661df5d3204809",
     "0xcaf6ba059d539a97646d47f0b9ddf843e138d215e2a12ca1f4585d386f7aec3a",
 ];
 
-// Previous package IDs for testnet (add when available)
-const TESTNET_PREVIOUS_PACKAGES: &[&str] = &[];
+const TESTNET_PREVIOUS_PACKAGES: &[&str] = &[
+    "0xc483dba510597205749f2e8410c23f19be31a710aef251f353bc1b97755efd4d",
+    "0x5da5bbf6fb097d108eaf2c2306f88beae4014c90a44b95c7e76a6bfccec5f5ee",
+    "0xa3886aaa8aa831572dd39549242ca004a438c3a55967af9f0387ad2b01595068",
+    "0x9592ac923593f37f4fed15ee15f760ebd4c39729f53ee3e8c214de7a17157769",
+    "0x984757fc7c0e6dd5f15c2c66e881dd6e5aca98b725f3dbd83c445e057ebb790a",
+    "0xfb28c4cbc6865bd1c897d26aecbe1f8792d1509a20ffec692c800660cbec6982",
+];
 const TESTNET_CURRENT_PACKAGE: &str =
     "0x16c4e050b9b19b25ce1365b96861bc50eb7e58383348a39ea8a8e1d063cfef73";
 
