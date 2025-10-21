a4feac4798569739050d1f993bc73716b064b550
Sam Barani <sam.barani@sui.io>
2025-10-21 15:21:22 -0500

    Add configurable RPC url for indexer server

--- docker/deepbook-server/entry.sh
+++ docker/deepbook-server/entry.sh
@@ -3,4 +3,9 @@
 export RUST_BACKTRACE=1
 export RUST_LOG=debug
 
-/opt/mysten/bin/deepbook-server --database-url "$DATABASE_URL" --rpc-url "$RPC_URL"
+/opt/mysten/bin/deepbook-server \
+  --database-url "$DATABASE_URL" \
+  --rpc-url "$RPC_URL" \
+  --deepbook-package-id "$DEEPBOOK_PACKAGE_ID" \
+  --deep-token-package-id "$DEEP_TOKEN_PACKAGE_ID" \
+  --deep-treasury-id "$DEEP_TREASURY_ID"
