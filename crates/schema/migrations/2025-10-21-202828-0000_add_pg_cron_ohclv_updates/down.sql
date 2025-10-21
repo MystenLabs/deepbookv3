d0fcaee1f0070ccf3abe3f14337453243722ca63
Sam Barani <sam.barani@sui.io>
2025-10-21 18:00:02 -0500

    add pg cron docs, remove migration

--- crates/schema/migrations/2025-10-21-202828-0000_add_pg_cron_ohclv_updates/down.sql
+++
@@ -1,4 +0,0 @@
-SELECT cron.unschedule('update_ohclv_1m_recent');
-SELECT cron.unschedule('update_ohclv_1d_recent');
-SELECT cron.unschedule('refresh_ohclv_1m_full');
-SELECT cron.unschedule('refresh_ohclv_1d_full');
