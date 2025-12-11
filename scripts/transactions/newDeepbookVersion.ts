248a38ef59b7d7280e844dd826e615d15f832cf7
Tony Lee <chunglee2018@u.northwestern.edu>
2025-12-11 11:52:59 -0500

    deepboo mvr

---
+++ scripts/transactions/newDeepbookVersion.ts
@@ -0,0 +1,53 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { namedPackagesPlugin, Transaction } from "@mysten/sui/transactions";
+import { prepareMultisigTx } from "../utils/utils";
+
+export type Network = "mainnet" | "testnet" | "devnet" | "localnet";
+
+const mainnetPlugin = namedPackagesPlugin({
+  url: "https://mainnet.mvr.mystenlabs.com",
+});
+(async () => {
+  const env = "mainnet";
+  const transaction = new Transaction();
+  transaction.addSerializationPlugin(mainnetPlugin);
+
+  // appcap holding address
+  const holdingAddress =
+    "0xd0ec0b201de6b4e7f425918bbd7151c37fc1b06c59b3961a2a00db74f6ea865e";
+
+  const deepbookPackageInfo =
+    "0x4874e126c490e495ff7490523841bdba57e2a01ed36db7610f07d417c8b5a988";
+
+  const data = {
+    repository: "https://github.com/MystenLabs/deepbookv3",
+    packageInfo: deepbookPackageInfo,
+    sha: "v4.0.0",
+    version: "4",
+    path: "packages/deepbook",
+  };
+
+  const git = transaction.moveCall({
+    target: `@mvr/metadata::git::new`,
+    arguments: [
+      transaction.pure.string(data.repository),
+      transaction.pure.string(data.path),
+      transaction.pure.string(data.sha),
+    ],
+  });
+
+  transaction.moveCall({
+    target: `@mvr/metadata::package_info::set_git_versioning`,
+    arguments: [
+      transaction.object(data.packageInfo),
+      transaction.pure.u64(data.version),
+      git,
+    ],
+  });
+
+  let res = await prepareMultisigTx(transaction, env, holdingAddress);
+
+  console.dir(res, { depth: null });
+})();
