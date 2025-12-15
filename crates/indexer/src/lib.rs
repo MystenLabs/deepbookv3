8c67b2f6c60e118265992494b6c6b51ca547b850
Tony Lee <tony.lee@mystenlabs.com>
2025-12-15 17:39:19 -0500

    Indexer IDs (#728)
    * update ids
    
    * new id
    
    * cargo fmt

--- crates/indexer/src/lib.rs
+++ crates/indexer/src/lib.rs
@@ -28,7 +28,9 @@ const TESTNET_PACKAGES: &[&str] = &[
     "0x9592ac923593f37f4fed15ee15f760ebd4c39729f53ee3e8c214de7a17157769",
     "0x984757fc7c0e6dd5f15c2c66e881dd6e5aca98b725f3dbd83c445e057ebb790a",
     "0xfb28c4cbc6865bd1c897d26aecbe1f8792d1509a20ffec692c800660cbec6982",
-    "0x926c446869fa175ec3b0dbf6c4f14604d86a415c1fccd8c8f823cfc46a29baed", // Latest
+    "0x926c446869fa175ec3b0dbf6c4f14604d86a415c1fccd8c8f823cfc46a29baed",
+    "0xa0936c6ea82fbfc0356eedc2e740e260dedaaa9f909a0715b1cc31e9a8283719",
+    "0x9ae1cbfb7475f6a4c2d4d3273335459f8f9d265874c4d161c1966cdcbd4e9ebc", // Latest
 ];
 
 // Mainnet margin package is not yet deployed - using placeholder
@@ -37,7 +39,8 @@ const TESTNET_PACKAGES: &[&str] = &[
 const MAINNET_MARGIN_PACKAGES: &[&str] = &[NOT_MAINNET_PACKAGE];
 const TESTNET_MARGIN_PACKAGES: &[&str] = &[
     "0xb8620c24c9ea1a4a41e79613d2b3d1d93648d1bb6f6b789a7c8f261c94110e4b",
-    "0xf978cf2b601c24e40ef82b6e51512b448696b44cb014c0a1162422aa8b9cb811", // Latest
+    "0xf978cf2b601c24e40ef82b6e51512b448696b44cb014c0a1162422aa8b9cb811",
+    "0x16d781c327a919dc55390f5cc60d58c7ec4535bb317e88850961222bbd5d4d9e", // Latest
 ];
 
 // Module definitions
