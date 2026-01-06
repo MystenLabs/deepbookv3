1ba89515e4ad843f52e5499b856f5df19ca55635
Tony Lee <tony.lee@mystenlabs.com>
2026-01-06 11:01:14 -0500

    Bump core version (#763)
    * version bump
    
    * enable version

--- scripts/transactions/enableVersion.ts
+++ scripts/transactions/enableVersion.ts
@@ -9,7 +9,7 @@ import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 (async () => {
   // Update constant for env
   const env = "mainnet";
-  const versionToEnable = 5;
+  const versionToEnable = 6;
 
   const dbClient = new DeepBookClient({
     address: "0x0",
