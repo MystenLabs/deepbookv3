823861230f095af8a615554fa08e68a17f04748e
Tony Lee <tony.lee@mystenlabs.com>
2026-01-13 15:18:46 -0500

    Margin setup part 1 (#787)
    * deepbook setup
    
    * finish setup

--- scripts/transactions/marginPrep.ts
+++ scripts/transactions/marginPrep.ts
@@ -14,7 +14,7 @@ import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
   // Update constant for env
   const env = "mainnet";
   const marginMaintainerCap =
-    "0xfa7c092fac70a0c2f9e8245748449c087727646ffe80e08efe702e4114402c9e";
+    "0xf44fb36ebfe03ff7696f8c17723bbc6af3db1e5eff7944aa65d092575851ca72";
 
   const dbClient = new DeepBookClient({
     address: "0x0",
@@ -29,41 +29,41 @@ import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
   const tx = new Transaction();
 
-  // // 1. Enable Margin package in core deepbook
-  // dbClient.deepBookAdmin.authorizeMarginApp()(tx);
+  // 1. Enable Margin package in core deepbook
+  dbClient.deepBookAdmin.authorizeMarginApp()(tx);
 
-  // // 2. PauseCap distribution
-  // const pauseCap1 = dbClient.marginAdmin.mintPauseCap()(tx);
-  // const pauseCap2 = dbClient.marginAdmin.mintPauseCap()(tx);
-  // const pauseCap3 = dbClient.marginAdmin.mintPauseCap()(tx);
-  // tx.transferObjects(
-  //   [pauseCap1],
-  //   "0x517f822cd3c45a3ac3dbfab73c060d9a0d96bec7fffa204c341e7e0877c9787c"
-  // );
-  // tx.transferObjects(
-  //   [pauseCap2],
-  //   "0x1b71380623813c8aee2ab9a68d96c19d0e45fc872e8c22dd70dfedfb76cbb192"
-  // );
-  // tx.transferObjects(
-  //   [pauseCap3],
-  //   "0x7da4267928e568da4f64f5a80f5b63680f3c2e008f4f96f475b60ff1c48c0dcf"
-  // );
+  // 2. PauseCap distribution
+  const pauseCap1 = dbClient.marginAdmin.mintPauseCap()(tx);
+  const pauseCap2 = dbClient.marginAdmin.mintPauseCap()(tx);
+  const pauseCap3 = dbClient.marginAdmin.mintPauseCap()(tx);
+  tx.transferObjects(
+    [pauseCap1],
+    "0x517f822cd3c45a3ac3dbfab73c060d9a0d96bec7fffa204c341e7e0877c9787c"
+  );
+  tx.transferObjects(
+    [pauseCap2],
+    "0x1b71380623813c8aee2ab9a68d96c19d0e45fc872e8c22dd70dfedfb76cbb192"
+  );
+  tx.transferObjects(
+    [pauseCap3],
+    "0x7da4267928e568da4f64f5a80f5b63680f3c2e008f4f96f475b60ff1c48c0dcf"
+  );
 
-  // // 3. Mint maintainerCap
-  // const maintainerCap = dbClient.marginAdmin.mintMaintainerCap()(tx);
-  // tx.transferObjects([maintainerCap], adminCapOwner[env]);
+  // 3. Mint maintainerCap
+  const maintainerCap = dbClient.marginAdmin.mintMaintainerCap()(tx);
+  tx.transferObjects([maintainerCap], adminCapOwner[env]);
 
-  // // 4. Pyth Config
-  // const pythConfig = dbClient.marginAdmin.newPythConfig(
-  //   [
-  //     { coinKey: "SUI", maxConfBps: 300, maxEwmaDifferenceBps: 1500 }, // maxConfBps: 3%, maxEwmaDifferenceBps: 15%
-  //     { coinKey: "USDC", maxConfBps: 100, maxEwmaDifferenceBps: 500 }, // maxConfBps: 1%, maxEwmaDifferenceBps: 5%
-  //     { coinKey: "DEEP", maxConfBps: 500, maxEwmaDifferenceBps: 3000 }, // maxConfBps: 5%, maxEwmaDifferenceBps: 30%
-  //     { coinKey: "WAL", maxConfBps: 500, maxEwmaDifferenceBps: 3000 }, // maxConfBps: 5%, maxEwmaDifferenceBps: 30%
-  //   ],
-  //   30 // maxAgeSeconds: 30 seconds
-  // )(tx);
-  // dbClient.marginAdmin.addConfig(pythConfig)(tx);
+  // 4. Pyth Config
+  const pythConfig = dbClient.marginAdmin.newPythConfig(
+    [
+      { coinKey: "SUI", maxConfBps: 300, maxEwmaDifferenceBps: 1500 }, // maxConfBps: 3%, maxEwmaDifferenceBps: 15%
+      { coinKey: "USDC", maxConfBps: 100, maxEwmaDifferenceBps: 500 }, // maxConfBps: 1%, maxEwmaDifferenceBps: 5%
+      { coinKey: "DEEP", maxConfBps: 500, maxEwmaDifferenceBps: 3000 }, // maxConfBps: 5%, maxEwmaDifferenceBps: 30%
+      { coinKey: "WAL", maxConfBps: 500, maxEwmaDifferenceBps: 3000 }, // maxConfBps: 5%, maxEwmaDifferenceBps: 30%
+    ],
+    30 // maxAgeSeconds: 30 seconds
+  )(tx);
+  dbClient.marginAdmin.addConfig(pythConfig)(tx);
 
   // 5. Create margin pools
   const USDCprotocolConfig = dbClient.marginMaintainer.newProtocolConfig(
