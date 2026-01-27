50888bc71609fa5cdd37b420028571eccaf60c62
Tony Lee <tony.lee@mystenlabs.com>
2026-01-27 13:45:04 -0500

    Update pool configs (#819)
    * update interest rates
    
    * update rate limit and capacity
    
    * maintainer cap

--- scripts/transactions/updateInterestRates.ts
+++ scripts/transactions/updateInterestRates.ts
@@ -31,105 +31,110 @@ import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 
   const tx = new Transaction();
 
-  dbClient.marginMaintainer.updateInterestParams(
-    "USDC",
-    tx.object(usdcMarginPoolCapID[env]),
-    {
-      baseRate: 0,
-      baseSlope: 0.15,
-      optimalUtilization: 0.8,
-      excessSlope: 5,
-    },
-  )(tx);
+  // dbClient.marginMaintainer.updateInterestParams(
+  //   "USDC",
+  //   tx.object(usdcMarginPoolCapID[env]),
+  //   {
+  //     baseRate: 0,
+  //     baseSlope: 0.15,
+  //     optimalUtilization: 0.8,
+  //     excessSlope: 5,
+  //   },
+  // )(tx);
 
   dbClient.marginMaintainer.updateMarginPoolConfig(
     "USDC",
     tx.object(usdcMarginPoolCapID[env]),
+    {
+      supplyCap: 2_000_000,
+      maxUtilizationRate: 0.9,
+      referralSpread: 0.2,
+      minBorrow: 0.1,
+      rateLimitCapacity: 400_000,
+      rateLimitRefillRatePerMs: 0.018518, // 400_000 / 21_600_000 (6 hours)
+      rateLimitEnabled: true,
+    },
+  )(tx);
+
+  // dbClient.marginMaintainer.updateInterestParams(
+  //   "SUI",
+  //   tx.object(suiMarginPoolCapID[env]),
+  //   {
+  //     baseRate: 0.03,
+  //     baseSlope: 0.2,
+  //     optimalUtilization: 0.8,
+  //     excessSlope: 5,
+  //   },
+  // )(tx);
+
+  dbClient.marginMaintainer.updateMarginPoolConfig(
+    "SUI",
+    tx.object(suiMarginPoolCapID[env]),
     {
       supplyCap: 1_000_000,
       maxUtilizationRate: 0.9,
       referralSpread: 0.2,
       minBorrow: 0.1,
       rateLimitCapacity: 200_000,
-      rateLimitRefillRatePerMs: 0.009259, // 200_000 / 21_600_000 (6 hours)
+      rateLimitRefillRatePerMs: 0.00925926, // 200_000 / 21_600_000 (6 hours)
       rateLimitEnabled: true,
     },
   )(tx);
 
-  dbClient.marginMaintainer.updateInterestParams(
-    "SUI",
-    tx.object(suiMarginPoolCapID[env]),
-    {
-      baseRate: 0.03,
-      baseSlope: 0.2,
-      optimalUtilization: 0.8,
-      excessSlope: 5,
-    },
-  )(tx);
+  // dbClient.marginMaintainer.updateInterestParams(
+  //   "DEEP",
+  //   tx.object(deepMarginPoolCapID[env]),
+  //   {
+  //     baseRate: 0.05,
+  //     baseSlope: 0.25,
+  //     optimalUtilization: 0.8,
+  //     excessSlope: 5,
+  //   },
+  // )(tx);
 
-  dbClient.marginMaintainer.updateMarginPoolConfig(
-    "SUI",
-    tx.object(suiMarginPoolCapID[env]),
-    {
-      supplyCap: 500_000,
-      maxUtilizationRate: 0.9,
-      referralSpread: 0.2,
-      minBorrow: 0.1,
-      rateLimitCapacity: 100_000,
-      rateLimitRefillRatePerMs: 0.00462963, // 100_000 / 21_600_000 (6 hours)
-      rateLimitEnabled: true,
-    },
-  )(tx);
+  // dbClient.marginMaintainer.updateMarginPoolConfig(
+  //   "DEEP",
+  //   tx.object(deepMarginPoolCapID[env]),
+  //   {
+  //     supplyCap: 20_000_000,
+  //     maxUtilizationRate: 0.9,
+  //     referralSpread: 0.2,
+  //     minBorrow: 0.1,
+  //     rateLimitCapacity: 4_000_000,
+  //     rateLimitRefillRatePerMs: 0.185185, // 4_000_000 / 21_600_000 (6 hours)
+  //     rateLimitEnabled: true,
+  //   },
+  // )(tx);
 
-  dbClient.marginMaintainer.updateInterestParams(
-    "DEEP",
-    tx.object(deepMarginPoolCapID[env]),
-    {
-      baseRate: 0.05,
-      baseSlope: 0.25,
-      optimalUtilization: 0.8,
-      excessSlope: 5,
-    },
-  )(tx);
+  // dbClient.marginMaintainer.updateInterestParams(
+  //   "WAL",
+  //   tx.object(walMarginPoolCapID[env]),
+  //   {
+  //     baseRate: 0.05,
+  //     baseSlope: 0.25,
+  //     optimalUtilization: 0.8,
+  //     excessSlope: 5,
+  //   },
+  // )(tx);
 
-  dbClient.marginMaintainer.updateMarginPoolConfig(
-    "DEEP",
-    tx.object(deepMarginPoolCapID[env]),
-    {
-      supplyCap: 20_000_000,
-      maxUtilizationRate: 0.9,
-      referralSpread: 0.2,
-      minBorrow: 0.1,
-      rateLimitCapacity: 4_000_000,
-      rateLimitRefillRatePerMs: 0.185185, // 4_000_000 / 21_600_000 (6 hours)
-      rateLimitEnabled: true,
-    },
-  )(tx);
-
-  dbClient.marginMaintainer.updateInterestParams(
-    "WAL",
-    tx.object(walMarginPoolCapID[env]),
-    {
-      baseRate: 0.05,
-      baseSlope: 0.25,
-      optimalUtilization: 0.8,
-      excessSlope: 5,
-    },
-  )(tx);
-
-  dbClient.marginMaintainer.updateMarginPoolConfig(
-    "WAL",
-    tx.object(walMarginPoolCapID[env]),
-    {
-      supplyCap: 7_000_000,
-      maxUtilizationRate: 0.9,
-      referralSpread: 0.2,
-      minBorrow: 0.1,
-      rateLimitCapacity: 1_400_000,
-      rateLimitRefillRatePerMs: 0.064814815, // 1_400_000 / 21_600_000 (6 hours)
-      rateLimitEnabled: true,
-    },
-  )(tx);
+  // dbClient.marginMaintainer.updateMarginPoolConfig(
+  //   "WAL",
+  //   tx.object(walMarginPoolCapID[env]),
+  //   {
+  //     supplyCap: 7_000_000,
+  //     maxUtilizationRate: 0.9,
+  //     referralSpread: 0.2,
+  //     minBorrow: 0.1,
+  //     rateLimitCapacity: 1_400_000,
+  //     rateLimitRefillRatePerMs: 0.064814815, // 1_400_000 / 21_600_000 (6 hours)
+  //     rateLimitEnabled: true,
+  //   },
+  // )(tx);
+  const maintainerCap = dbClient.marginAdmin.mintMaintainerCap()(tx);
+  tx.transferObjects(
+    [maintainerCap],
+    "0x6b9f717104d04a5e53cbcd95213c6fbe3616809d396ec4c31076f7dddc497362",
+  );
 
   let res = await prepareMultisigTx(tx, env, adminCapOwner[env]);
 
