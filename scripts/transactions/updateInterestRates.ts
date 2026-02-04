2815b9ff380e1f7f3135dbe67dba186c1f17d58e
Tony Lee <chunglee2018@u.northwestern.edu>
2026-02-04 11:17:09 -0500

    increase supply cap

--- scripts/transactions/updateInterestRates.ts
+++ scripts/transactions/updateInterestRates.ts
@@ -97,12 +97,12 @@ import { SuiGrpcClient } from "@mysten/sui/grpc";
     "DEEP",
     tx.object(deepMarginPoolCapID[env]),
     {
-      supplyCap: 20_000_000,
+      supplyCap: 30_000_000,
       maxUtilizationRate: 0.9,
       referralSpread: 0.2,
       minBorrow: 0.1,
-      rateLimitCapacity: 4_000_000,
-      rateLimitRefillRatePerMs: 0.185185, // 4_000_000 / 21_600_000 (6 hours)
+      rateLimitCapacity: 6_000_000,
+      rateLimitRefillRatePerMs: 0.277778, // 6_000_000 / 21_600_000 (6 hours)
       rateLimitEnabled: true,
     },
   )(tx);
